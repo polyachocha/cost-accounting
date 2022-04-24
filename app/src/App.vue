49eade030b1f434d9784b3ddd323ad1e31433c7c
Anna Polyakova <polyakova.as@mail.ru>
2022-04-24 18:21:25 +0300

    homework for lesson 3

---
+++ app/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <div class="title">My personal costs</div>
+    </header>
+    <main>
+      <PaymentsDisplay :items="paymentsList" />
+      <AddPaymentForm @addNewPayment="addNewPayment" />
+    </main>
+  </div>
+</template>
+
+<script>
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
+import AddPaymentForm from "./components/AddPaymentForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    PaymentsDisplay,
+    AddPaymentForm,
+  },
+  data() {
+    return {
+      paymentsList: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      return [
+        {
+          date: "28.03.2022",
+          category: "Food",
+          value: "169",
+        },
+        {
+          date: "29.03.2022",
+          category: "Transport",
+          value: "360",
+        },
+        {
+          date: "02.04.2022",
+          category: "Food",
+          value: "531",
+        },
+      ];
+    },
+    addNewPayment(data) {
+      this.paymentsList.push(data);
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
+  },
+};
+</script>
+<style>
+.title {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ app/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue/dist/vue.js'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: {
+    App,
+  }
+})
