a3f0f43655c9842fc0084d46fb413d7dfad2e394
Anna Polyakova <polyakova.as@mail.ru>
2022-06-01 22:00:25 +0300

    homework for lesson7

--- app/src/views/PageAbout.vue
+++ app/src/views/PageAbout.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1>About app</h1>
+    <CalcComp />
   </div>
 </template>
 <script>
+import CalcComp from "../components/CalcComp.vue";
 export default {
   name: "PageAbout",
+  components: {
+    CalcComp,
+  },
 };
 </script>
 <style scoped>
